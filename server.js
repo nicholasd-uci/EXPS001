59575aed34fa4a8afe04bec070bfd0d360135aff
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-22 22:49:14 -0800

    updated server file to be tested

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 // step 1
 const express = require('express')
-const path = require('path')
+const { join } = require('path')
 
 // step 2 - call 'express' like a function
 const app = express()
 
 app.get('/food', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/foods.html'))
+    res.sendFile(join(__dirname, 'public/foods.html'))
 })
 
 app.get('/games', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/games.html'))
+    res.sendFile(join(__dirname, 'public/games.html'))
 })
 
 app.get('/movies', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/movies.html'))
+    res.sendFile(join(__dirname, 'public/movies.html'))
 })
 
 app.get('/songs', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/songs.html'))
-})
-
-app.post('/something', (req, res) => {
-    res.send('POST')
+    res.sendFile(join(__dirname, 'public/songs.html'))
 })
 
 // step 3 - listen
